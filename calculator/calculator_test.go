fafcc11c694e8fa2278e96bfe52f7bd8757f0fd0
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2024-11-24 23:13:24 -0300

    Add another test for Divide
    Divide isn't implemented yet so who knows what's gonna happen. Stay tuned.

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -64,3 +64,26 @@ func TestMultiply(t *testing.T) {
 		}
 	}
 }
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 1},
+		{a: -1, b: -1, want: 1},
+		{a: 10, b: 2, want: 5},
+	}
+
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if err != nil {
+			t.Fatalf("want no error for valid input, got %v", err)
+		}
+		if tc.want != got {
+			t.Errorf("Divide(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
