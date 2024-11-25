9a4405caa544111e05926b55fc83e4fee6354066
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2024-11-24 22:39:36 -0300

    Add test cases to Subtract and Multiply
    This commit does the same thing to the other two functions of the calculator. Nothing much.

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -27,18 +27,40 @@ func TestAdd(t *testing.T) {
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 0},
+		{a: 3, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
-	var want float64 = 9
-	got := calculator.Multiply(3, 3)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 3, b: 1, want: 3},
+		{a: 5, b: 5, want: 25},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
