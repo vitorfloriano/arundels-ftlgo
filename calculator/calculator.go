31ac19d5d3692a0fee5b19794c127aec37a2c079
Vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-11-28 16:11:52 -0300

    Add Sqrt function and its tests
    This commit adds the Sqrt function along with the tests needed. Something might be broken, though.

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,5 +1,10 @@
 package calculator
 
+import (
+	"errors"
+	"math"
+	)
+
 func Add(a, b float64) float64 {
 	return a + b
 }
@@ -14,5 +19,15 @@ func Multiply(a, b float64) float64 {
 
 
 func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero not allowed")
+	}
 	return a / b, nil
+}
+
+func Sqrt(a float64) (float64, error) {
+	if a > 0 {
+		return 0, errors.New("square root of zero or negative numbers not allowed")
+	}
+	return math.Sqrt(a), nil
 }
