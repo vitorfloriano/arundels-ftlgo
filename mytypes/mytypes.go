d1aad536250b22b3ecb6402f55757861c408d50b
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2024-12-08 17:30:22 -0300

    Add MyBuilder to types and Hello to methods
    So I created this new type MyBuilder based on the strings.Builder type, but I had to reimplement the methods in it, because there's no inheritance.

--- mytypes/mytypes.go
+++ mytypes/mytypes.go
@@ -1,7 +1,10 @@
 package mytypes
 
+import "strings"
+
 type MyInt int
 type MyString string
+type MyBuilder strings.Builder
 
 func (i MyInt) Twice() MyInt {
 	return i * 2
@@ -9,4 +12,8 @@ func (i MyInt) Twice() MyInt {
 
 func (s MyString) Len() int {
 	return len(s)
+}
+
+func (mb MyBuilder) Hello() string {
+	return "Hello, Gophers!"
 }
--- mytypes/mytypes_test.go
+++ mytypes/mytypes_test.go
@@ -3,6 +3,7 @@ package mytypes_test
 import (
 	"mytypes"
 	"testing"
+	"strings"
 )
 
 func TestTwice(t *testing.T) {
@@ -24,3 +25,30 @@ func TestMyStringLen(t *testing.T) {
 		t.Errorf("want %d, got %d", want, got)
 	}
 }	
+
+func TestStringsBuilder(t *testing.T) {
+	t.Parallel()
+	var sb strings.Builder
+	sb.WriteString("Hello, ")
+	sb.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := sb.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+	wantLen := 15
+	gotLen := sb.Len()
+	if wantLen != gotLen {
+		t.Errorf("%q: want %q, got %q", sb.String(), wantLen, gotLen)
+	}
+}
+
+func TestMyBuilderHello(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyBuilder
+	want := "Hello, Gophers!"
+	got := mb.Hello()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
