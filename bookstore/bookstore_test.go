f25fe4161c224f168e6ab64c0050784d3d25e03a
Vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-12-09 15:52:40 -0300

    Add methods to the Category field
    WIP

--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -150,3 +150,30 @@ func TestSetPriceCentsInvalid(t *testing.T) {
 		t.Fatal("want error setting invalid price -1, got nil")
 	}
 }
+
+func TestSetCategoryInvalid(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title: "For the Love of Go",
+	}
+	err := b.SetCategory("bogus")
+	if err == nil {
+		t.Fatal("want error for invalid category, got nil")
+	}	
+}
+
+func TestSetCategory(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title: "For the Love of Go",
+	}
+	want := "Autobiography"
+	err := b.SetCategory(want)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := b.Category()
+	if want != got {
+		t.Errorf("want category %q, got %q", want, got)
+	}
+}
